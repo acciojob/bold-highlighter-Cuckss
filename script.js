79dd02550030d2bfa8032d662c62ac387b1efbf5
Cuckss <122050270+Cuckss@users.noreply.github.com>
2023-07-24 14:45:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+ const strongList=document.getElementsByTagName("strong");
 function highlight() {
-    //Write your code here
+   
+	for(let i=0;i<strongList.length;i++){
+		strongList[i].style.color="green";
+	}
 
 
 }
 
 
 function return_normal() {
-    //Write your code here
+  for(let i=0;i<strongList.length;i++){
+		strongList[i].style.color="black";
+	}
 
     
 }
